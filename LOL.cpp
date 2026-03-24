28edff7c745e796297003461580117a653057779
Artemnoellem <Artemnoellem>
2026-03-24 18:37:15 +0000

    Добавил расчёт прямоугольника в LOL.cpp

---
+++ LOL.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main() {
+    double a, b;
+    
+    cout << "Введите сторону А: ";
+    cin >> a;
+    cout << "Введите сторону B: ";
+    cin >> b;
+
+    //Проверка
+
+    if (a <= 0 || b <= 0) {
+        cout << " ОШИБКА! СТОРОНА НЕ МОЖЕТ РАВНЯТЬСЯ НУЛЮ ";
+        return 1;
+    }
+
+    //Считаем параметры
+
+    double per = 2 * (a + b);
+    double area = a* b;
+    double dio = sqrt(a * a + b * b);
+
+    //Вывод
+
+    cout << "Результат" << endl;
+    cout << "Периметр: " << per << endl;
+    cout << "Площадь:  " << area << endl;
+    cout << "Диалональ:  " << dio << endl;
+
+    return 0;
+    
+}
